baa2d3aa3c1ebc456fac5f0abab774d1e2e9eebf
Emona Santiago <emonasantiago1207@gmail.com>
2023-05-08 12:27:17 +0200

    added the contact page

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,33 @@
+<script></script>
+
+<section class="contact">
+  <div class="hero">
+    <h1>Doe met ons mee?</h1>
+    <p>
+      Als deelnemer aan het pact vragen wij u om met ons mee te denken over en
+      mee te werken aan het vormgeven van de aangepaste opleiding. Daarnaast
+      vragen we u om uw invloed en netwerk in te zetten om werkgevers te
+      enthousiasmeren mensen met een visuele beperking in hun organisatie op te
+      nemen.
+    </p>
+    <h3>Ideeën of suggesties?</h3>
+    <button type="button">Stuur ons een mail</button>
+  </div>
+  <img src="/" alt="image">
+</section>
+
+<style>
+  .contact {
+    margin-left: 6em;
+  }
+
+  .hero {
+    width: 60em;
+  }
+
+ 
+
+
+
+ 
+</style>
