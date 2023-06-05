2863d1f8951ff4b0c7d76ceb898b1b014ada1cfd
DphnZwp <zwuupdaphne@gmail.com>
2023-06-05 11:46:42 +0200

    netlify test

--- admin/admin-script.js
+++ admin/admin-script.js
@@ -1,47 +1,42 @@
 fetch('/content/index.md')
   .then(response => response.text())
   .then(content => {
-    CMS.registerEditorComponent({
-      id: 'markdown',
-      label: 'Markdown',
-      pattern: /^---\n.*?---\n(.*)/s,
-      fromBlock: match => match && match[1],
-      toBlock: content => `---\n${content}---\n`,
-      toPreview: content => `---\n${content}---\n`,
+    window.CMS.registerPreviewTemplate('home', ({ entry }) => {
+      const markdownContent = entry.getIn(['data', 'content']);
+      const updatedContent = markdownContent || content;
+      return `
+        <div>
+          <textarea id="cms-content" name="content" style="display: none">${updatedContent}</textarea>
+        </div>
+      `;
     });
-    CMS.init();
-    const editor = window.CMS.getEditorByFieldPath('content');
-    editor.setValue(content);
   });
 
-	fetch('/content/over-ons.md')
+  fetch('/content/over-ons.md')
   .then(response => response.text())
   .then(content => {
-    CMS.registerEditorComponent({
-      id: 'markdown',
-      label: 'Markdown',
-      pattern: /^---\n.*?---\n(.*)/s,
-      fromBlock: match => match && match[1],
-      toBlock: content => `---\n${content}---\n`,
-      toPreview: content => `---\n${content}---\n`,
+    window.CMS.registerPreviewTemplate('home', ({ entry }) => {
+      const markdownContent = entry.getIn(['data', 'content']);
+      const updatedContent = markdownContent || content;
+      return `
+        <div>
+          <textarea id="cms-content" name="content" style="display: none">${updatedContent}</textarea>
+        </div>
+      `;
     });
-    CMS.init();
-    const editor = window.CMS.getEditorByFieldPath('content');
-    editor.setValue(content);
   });
 
-	fetch('/content/contact.md')
+  fetch('/content/contact.md')
   .then(response => response.text())
   .then(content => {
-    CMS.registerEditorComponent({
-      id: 'markdown',
-      label: 'Markdown',
-      pattern: /^---\n.*?---\n(.*)/s,
-      fromBlock: match => match && match[1],
-      toBlock: content => `---\n${content}---\n`,
-      toPreview: content => `---\n${content}---\n`,
+    window.CMS.registerPreviewTemplate('home', ({ entry }) => {
+      const markdownContent = entry.getIn(['data', 'content']);
+      const updatedContent = markdownContent || content;
+      return `
+        <div>
+          <textarea id="cms-content" name="content" style="display: none">${updatedContent}</textarea>
+        </div>
+      `;
     });
-    CMS.init();
-    const editor = window.CMS.getEditorByFieldPath('content');
-    editor.setValue(content);
   });
+  
