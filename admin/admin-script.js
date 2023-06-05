835207fb7b37a6db6d681ac68be8d45d0c1add80
DphnZwp <zwuupdaphne@gmail.com>
2023-06-05 11:39:27 +0200

    js in admin

--- admin/admin-script.js
+++ admin/admin-script.js
@@ -1,47 +1,47 @@
-// fetch('/content/index.md')
-//   .then(response => response.text())
-//   .then(content => {
-//     CMS.registerEditorComponent({
-//       id: 'markdown',
-//       label: 'Markdown',
-//       pattern: /^---\n.*?---\n(.*)/s,
-//       fromBlock: match => match && match[1],
-//       toBlock: content => `---\n${content}---\n`,
-//       toPreview: content => `---\n${content}---\n`,
-//     });
-//     CMS.init();
-//     const editor = window.CMS.getEditorByFieldPath('content');
-//     editor.setValue(content);
-//   });
+fetch('/content/index.md')
+  .then(response => response.text())
+  .then(content => {
+    CMS.registerEditorComponent({
+      id: 'markdown',
+      label: 'Markdown',
+      pattern: /^---\n.*?---\n(.*)/s,
+      fromBlock: match => match && match[1],
+      toBlock: content => `---\n${content}---\n`,
+      toPreview: content => `---\n${content}---\n`,
+    });
+    CMS.init();
+    const editor = window.CMS.getEditorByFieldPath('content');
+    editor.setValue(content);
+  });
 
-// 	fetch('/content/over-ons.md')
-//   .then(response => response.text())
-//   .then(content => {
-//     CMS.registerEditorComponent({
-//       id: 'markdown',
-//       label: 'Markdown',
-//       pattern: /^---\n.*?---\n(.*)/s,
-//       fromBlock: match => match && match[1],
-//       toBlock: content => `---\n${content}---\n`,
-//       toPreview: content => `---\n${content}---\n`,
-//     });
-//     CMS.init();
-//     const editor = window.CMS.getEditorByFieldPath('content');
-//     editor.setValue(content);
-//   });
+	fetch('/content/over-ons.md')
+  .then(response => response.text())
+  .then(content => {
+    CMS.registerEditorComponent({
+      id: 'markdown',
+      label: 'Markdown',
+      pattern: /^---\n.*?---\n(.*)/s,
+      fromBlock: match => match && match[1],
+      toBlock: content => `---\n${content}---\n`,
+      toPreview: content => `---\n${content}---\n`,
+    });
+    CMS.init();
+    const editor = window.CMS.getEditorByFieldPath('content');
+    editor.setValue(content);
+  });
 
-// 	fetch('/content/contact.md')
-//   .then(response => response.text())
-//   .then(content => {
-//     CMS.registerEditorComponent({
-//       id: 'markdown',
-//       label: 'Markdown',
-//       pattern: /^---\n.*?---\n(.*)/s,
-//       fromBlock: match => match && match[1],
-//       toBlock: content => `---\n${content}---\n`,
-//       toPreview: content => `---\n${content}---\n`,
-//     });
-//     CMS.init();
-//     const editor = window.CMS.getEditorByFieldPath('content');
-//     editor.setValue(content);
-//   });
+	fetch('/content/contact.md')
+  .then(response => response.text())
+  .then(content => {
+    CMS.registerEditorComponent({
+      id: 'markdown',
+      label: 'Markdown',
+      pattern: /^---\n.*?---\n(.*)/s,
+      fromBlock: match => match && match[1],
+      toBlock: content => `---\n${content}---\n`,
+      toPreview: content => `---\n${content}---\n`,
+    });
+    CMS.init();
+    const editor = window.CMS.getEditorByFieldPath('content');
+    editor.setValue(content);
+  });
