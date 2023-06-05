7ebfc0dcf51e3e35f35f4af968cf275706fef5f1
DphnZwp <zwuupdaphne@gmail.com>
2023-06-05 13:47:59 +0200

    netlify test

--- _site/admin/admin-script.js
+++ _site/admin/admin-script.js
@@ -1,40 +1,49 @@
 window.CMS_MANUAL_INIT = true;
 
-window.addEventListener("DOMContentLoaded", () => {
-  fetch('/path/to/your/content/index.md')
-    .then(response => response.text())
-    .then(content => {
-      const config = {
-        backend: {
-          name: 'git-gateway',
-          branch: 'main',
-        },
-        media_folder: '_site/images',
-        public_folder: '/images',
-        collections: [
-          {
-            name: 'pages',
-            label: 'Pages',
-            files: [
-              {
-                name: 'home',
-                label: 'Home',
-                file: 'content/index.md',
-                fields: [
-                  {
-                    name: 'content',
-                    label: 'Content',
-                    widget: 'markdown',
-                    default: content,
-                  },
-                ],
-              },
-              // Add other pages (about, contact) similarly
-            ],
-          },
-        ],
-      };
+fetch('/content/index.md')
+  .then(response => response.text())
+  .then(content => {
+    document.addEventListener('DOMContentLoaded', () => {
+      const contentField = document.querySelector('textarea[name="fields[content]"]');
 
-      CMS.init({ config });
+      if (contentField) {
+        contentField.value = content;
+      }
     });
-});
+
+    const config = {
+      backend: {
+        name: 'git-gateway',
+        branch: 'main',
+      },
+      media_folder: '_site/images',
+      public_folder: '/images',
+      collections: [
+        {
+          name: 'pages',
+          label: 'Pages',
+          files: [
+            {
+              name: 'home',
+              label: 'Home',
+              file: 'content/index.md',
+              fields: [
+                {
+                  name: 'title',
+                  label: 'Title',
+                  widget: 'string',
+                },
+                {
+                  name: 'content',
+                  label: 'Content',
+                  widget: 'markdown',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    };
+
+    CMS.init({ config });
+  });
