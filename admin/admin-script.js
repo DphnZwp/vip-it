47cb558e2944199a2877eb271593845df528d65f
DphnZwp <zwuupdaphne@gmail.com>
2023-06-05 12:31:58 +0200

    trying to solve error in admin script

--- admin/admin-script.js
+++ admin/admin-script.js
@@ -2,61 +2,70 @@ window.CMS_MANUAL_INIT = true;
 
 fetch('/path/to/your/content/index.md')
   .then(response => response.text())
-  .then(content => {
-    const config = {
-      backend: {
-        name: 'git-gateway',
-        branch: 'main',
-      },
-      media_folder: '_site/images',
-      public_folder: '/images',
-      collections: [
-        {
-          name: 'pages',
-          label: 'Pages',
-          files: [
-            {
-              name: 'home',
-              label: 'Home',
-              file: 'content/index.md',
-              fields: [
+  .then(homeContent => {
+    fetch('/path/to/your/content/over-ons.md')
+      .then(response => response.text())
+      .then(overContent => {
+        fetch('/path/to/your/content/contact.md')
+          .then(response => response.text())
+          .then(contactContent => {
+            const config = {
+              backend: {
+                name: 'git-gateway',
+                branch: 'main',
+              },
+              media_folder: '_site/images',
+              public_folder: '/images',
+              collections: [
                 {
-                  name: 'content',
-                  label: 'Content',
-                  widget: 'markdown',
+                  name: 'pages',
+                  label: 'Pages',
+                  files: [
+                    {
+                      name: 'home',
+                      label: 'Home',
+                      file: 'content/index.md',
+                      fields: [
+                        {
+                          name: 'content',
+                          label: 'Content',
+                          widget: 'markdown',
+                          default: homeContent,
+                        },
+                      ],
+                    },
+                    {
+                      name: 'over',
+                      label: 'Over ons',
+                      file: 'content/over-ons.md',
+                      fields: [
+                        {
+                          name: 'content',
+                          label: 'Content',
+                          widget: 'markdown',
+                          default: overContent,
+                        },
+                      ],
+                    },
+                    {
+                      name: 'contact',
+                      label: 'Contact',
+                      file: 'content/contact.md',
+                      fields: [
+                        {
+                          name: 'content',
+                          label: 'Content',
+                          widget: 'markdown',
+                          default: contactContent,
+                        },
+                      ],
+                    },
+                  ],
                 },
               ],
-            },
-            {
-              name: 'over',
-              label: 'Over ons',
-              file: 'content/over-ons.md',
-              fields: [
-                {
-                  name: 'content',
-                  label: 'Content',
-                  widget: 'markdown',
-                },
-              ],
-            },
-            {
-              name: 'contact',
-              label: 'Contact',
-              file: 'content/contact.md',
-              fields: [
-                {
-                  name: 'content',
-                  label: 'Content',
-                  widget: 'markdown',
-                },
-              ],
-            },
-          ],
-        },
-      ],
-    };
+            };
 
-    config.collections[0].files[0].fields[0].default = content;
-
-    CMS.init({ config });
+            CMS.init({ config });
+          });
+      });
   });
