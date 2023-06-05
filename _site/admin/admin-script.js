2c0feafb4c10f818281f91f0f42e8c28420f5b60
DphnZwp <zwuupdaphne@gmail.com>
2023-06-05 13:23:49 +0200

    netlify test

--- _site/admin/admin-script.js
+++ _site/admin/admin-script.js
@@ -1,86 +1,40 @@
 window.CMS_MANUAL_INIT = true;
 
-fetch('/path/to/your/content/index.md')
-  .then(response => response.text())
-  .then(homeContent => {
-    fetch('/path/to/your/content/over-ons.md')
-      .then(response => response.text())
-      .then(overContent => {
-        fetch('/content/contact.md')
-          .then(response => response.text())
-          .then(contactContent => {
-            const config = {
-              backend: {
-                name: 'git-gateway',
-                branch: 'main',
+window.addEventListener("DOMContentLoaded", () => {
+  fetch('/path/to/your/content/index.md')
+    .then(response => response.text())
+    .then(content => {
+      const config = {
+        backend: {
+          name: 'git-gateway',
+          branch: 'main',
+        },
+        media_folder: '_site/images',
+        public_folder: '/images',
+        collections: [
+          {
+            name: 'pages',
+            label: 'Pages',
+            files: [
+              {
+                name: 'home',
+                label: 'Home',
+                file: 'content/index.md',
+                fields: [
+                  {
+                    name: 'content',
+                    label: 'Content',
+                    widget: 'markdown',
+                    default: content,
+                  },
+                ],
               },
-              media_folder: '_site/images',
-              public_folder: '/images',
-              collections: [
-                {
-                  name: 'pages',
-                  label: 'Pages',
-                  files: [
-                    {
-                      name: 'home',
-                      label: 'Home',
-                      file: 'content/index.md',
-                      fields: [
-                        {
-                          name: 'title',
-                          label: 'Title',
-                          widget: 'string',
-                        },
-                        {
-                          name: 'content',
-                          label: 'Content',
-                          widget: 'markdown',
-                          default: homeContent,
-                        },
-                      ],
-                    },
-                    {
-                      name: 'over',
-                      label: 'Over ons',
-                      file: 'content/over-ons.md',
-                      fields: [
-                        {
-                          name: 'title',
-                          label: 'Title',
-                          widget: 'string',
-                        },
-                        {
-                          name: 'content',
-                          label: 'Content',
-                          widget: 'markdown',
-                          default: overContent,
-                        },
-                      ],
-                    },
-                    {
-                      name: 'contact',
-                      label: 'Contact',
-                      file: 'content/contact.md',
-                      fields: [
-                        {
-                          name: 'title',
-                          label: 'Title',
-                          widget: 'string',
-                        },
-                        {
-                          name: 'content',
-                          label: 'Content',
-                          widget: 'markdown',
-                          default: contactContent,
-                        },
-                      ],
-                    },
-                  ],
-                },
-              ],
-            };
+              // Add other pages (about, contact) similarly
+            ],
+          },
+        ],
+      };
 
-            CMS.init({ config });
-          });
-      });
-  });
+      CMS.init({ config });
+    });
+});
