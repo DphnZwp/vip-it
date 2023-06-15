56efee0768f5934084e8ac0824f0ca8eec253a5c
DphnZwp <zwuupdaphne@gmail.com>
2023-06-15 20:50:17 +0200

    removed prevent default

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 document.addEventListener('keydown', function(event) {
 	if (event.ctrlKey && event.key === 'Home') {
-			event.preventDefault()
 			document.querySelector('.skip-nav-link').focus();
 	}
 });
